8a7c8c14823138ea6a3106c9d492aad3a5d9234b
mostafa <amgedtamer12345@gmail.com>
2024-08-13 14:00:16 +0300

    update auth

--- app/src/main/java/com/example/foodplanner/Presenters/LoginPresenter.java
+++ app/src/main/java/com/example/foodplanner/Presenters/LoginPresenter.java
@@ -1,22 +1,78 @@
 package com.example.foodplanner.Presenters;
 
+import static com.example.foodplanner.Util.Utilits.emailPattern;
+import static com.example.foodplanner.Util.Utilits.passwordPattern;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.example.foodplanner.R;
 import com.example.foodplanner.Util.IauthPresenter;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginPresenter implements IauthPresenter.IloginPresenter {
-   public static LoginPresenter presenter;
+    private Context  context;
+    private static LoginPresenter presenter;
+    private final FirebaseAuth firebaseAuth;
+    private  IauthPresenter.IloginPresenter.Icommuncate icommuncate;
     private LoginPresenter(){
-
+        firebaseAuth=FirebaseAuth.getInstance();
     }
 
     @Override
     public void Login(String email, String password) {
+        int res=checkEmail(email);
+        int res2=checkPassword(password);
+        if (res>=0 || res2>=0)
+            return;
+        boolean isEmailValid = email.matches(emailPattern);
+        boolean isPasswordValid = password.matches(passwordPattern);
+        if (!isEmailValid) {
+            icommuncate.errorEmailValidation(context.getString(R.string.email_is_invalid));
+        }
+        if (!isPasswordValid) {
+            icommuncate.errorEmailValidation(context.getString(R.string.password_is_invalid));
+        }
+        if (!isEmailValid||!isPasswordValid)
+            return;
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+          if (task.isSuccessful()){
+              icommuncate.Sucess();
+          }else{
+              icommuncate.Error(task.getException().getMessage());
+          }
+        });
 
     }
 
-    public static synchronized  LoginPresenter getInstance() {
-        if (presenter == null) {
+    @Override
+    public int checkEmail(String email) {
+        if (email.trim().isEmpty()) {
+            icommuncate.errorEmailValidation(context.getString(R.string.email_is_empty));
+            return -1;
+        }
+        return 0;
+    }
+
+    @Override
+    public int checkPassword(String password) {
+        if (password.trim().isEmpty()) {
+            icommuncate.errorPasswordValidation(context.getString(R.string.password_is_empty));
+            return -1;
+        }
+        return 0;
+
+    }
+
+    public static synchronized  LoginPresenter getInstance(Context context, IauthPresenter.IloginPresenter.Icommuncate icommuncate) {
+
+         if (presenter == null) {
             presenter= new LoginPresenter();
         }
+        presenter.context=context;
+        presenter.icommuncate= icommuncate;
         return presenter;
     }
+
 }
--- app/src/main/java/com/example/foodplanner/Util/IauthPresenter.java
+++ app/src/main/java/com/example/foodplanner/Util/IauthPresenter.java
@@ -3,9 +3,14 @@ package com.example.foodplanner.Util;
 public interface IauthPresenter {
     public interface IloginPresenter {
         void Login(String email, String password);
+        int checkEmail(String email);
+        int checkPassword(String password);
         interface Icommuncate{
             void Sucess();
             void Error(String error);
+            void errorEmailValidation(String error);
+            void errorPasswordValidation(String error);
+
         }
     }
 }
